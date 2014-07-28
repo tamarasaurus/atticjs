75457e74ac2c1eb56564f094c0cb0f5c6ae6dcef
Tamara Chahine <chahine.tamara@abc.net.au>
2014-07-28 16:36:17 +1000

    Add object support to storage

--- index.js
+++ index.js
@@ -4,61 +4,67 @@ var _ = require('underscore');
 
 module.exports = (function() {
 
-    var attic = function() {};
+	var attic = function() {};
 
-    var supported = function() {
-        var test = 'test';
-        try {
-            localStorage.setItem(test, test);
-            localStorage.removeItem(test);
-            return true;
-        } catch (e) {
-            return false;
-        }
-    };
+	var supported = function() {
+		var test = 'test';
+		try {
+			localStorage.setItem(test, test);
+			localStorage.removeItem(test);
+			return true;
+		} catch (e) {
+			return false;
+		}
+	};
 
-    attic.prototype = {
-        hasKey: function(key) {
-            return !_.isUndefined(localStorage.getItem(key)) && !_.isEmpty(localStorage.getItem(key));
-        },
-        find: function(key) {
-            if (this.hasKey(key)) {
-                return localStorage.getItem(key).split(',');
-            } else {
-                return [];
-            }
-        },
-        hasValue: function(key, value) {
-            return _.contains(this.find(key), value.toString());
-        },
-        store: function(value, key) {
-            if (this.hasKey(key)) {
-                if (!this.hasValue(key, value)) {
-                    var arr = this.find(key);
-                    arr.push(value);
-                    localStorage.setItem(key, arr.toString());
-                }
-            } else {
-                localStorage.setItem(key, value.toString());
-            }
-            return localStorage.getItem(key);
-        },
-        clean: function(value, key) {
-            var result = _.without(this.find(key), value.toString());
-            localStorage.setItem(key, result.toString());
-            return result;
-        },
-        cleanKey: function(key){
-            localStorage.removeItem(key);
-        }
+	attic.prototype = {
+		str: function(val) {
+			return JSON.stringify(val);
+		},
+		parseVal: function(val) {
+			return JSON.parse(val);
+		},
+		hasKey: function(key) {
+			return !_.isUndefined(localStorage.getItem(key)) && !_.isEmpty(localStorage.getItem(key));
+		},
+		find: function(key) {
+			if (this.hasKey(key)) {
+				return this.parseVal(localStorage.getItem(key));
+			} else {
+				return null;
+			}
+		},
+		hasValue: function(key, value) {
+			return _.contains(this.find(key), this.str(value));
+		},
+		store: function(value, key) {
+			if (this.hasKey(key)) {
+				if (!this.hasValue(key, value)) {
+					var arr = this.find(key);
+					arr.push(this.str(value));
+					localStorage.setItem(key, this.str(arr));
+				}
+			} else {
+				localStorage.setItem(key, this.str([this.str(value)]));
+			}
+			return localStorage.getItem(key);
+		},
+		clean: function(value, key) {
+			var result = _.without(this.find(key), this.str(value));
+			localStorage.setItem(key, JSON.stringify(result));
+			return result;
+		},
+		cleanKey: function(key) {
+			localStorage.removeItem(key);
+		}
 
-    };
+	};
 
-    if (supported()) {
-        return attic;
-    } else {
-        console.log('localstorage not supported');
-        return false;
-    }
+	if (supported()) {
+		return attic;
+	} else {
+		console.log('localstorage not supported');
+		return false;
+	}
 
 }());
