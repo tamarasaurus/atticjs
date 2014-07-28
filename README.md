bcae16b199b38d275da26f01e21bc0c622252427
Tamara Chahine <chahine.tamara@abc.net.au>
2014-07-28 11:21:50 +1000

    Update basic structure and readme

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ atticjs
 =======
 
 Small localstorage wrapper with prefixing
+
+
+credits
+=======
+
+Thanks to [Emma Chee](https://github.com/echee) for naming the project :)
--- index.js
+++ index.js
@@ -4,24 +4,47 @@ var _ = require('underscore');
 
 module.exports = (function() {
 
-    var attic = function(prefix) {
-        this.prefix = prefix;
-    };
+	var attic = function(prefix) {
+		this.prefix = prefix;
+	};
 
-    attic.prototype = {
-        store: function() {
-
+	attic.prototype = {
+        supported: function(){
+            var test = 'test';
+            try {
+                localStorage.setItem(test, test);
+                localStorage.removeItem(test);
+                return true;
+            } catch (e) {
+                return false;
+            }
         },
-        remove: function(){
+		get_key: function(key) {
+			// get a key from storage
+		},
+		get_value: function(key, value) {
+			// find a value in the key
+		},
+		store_key: function(key, value) {
+			// store a key value pair
+		},
+		remove_value: function(key, value) {
+			// remove a value from the key
+		},
+		remove_key: function(key) {
+            // remove a key from localstorage
+		},
+		empty: function() {
+			// remove all keys with the set prefix
+		}
 
-        },
-        clean: function(){
+	};
 
-        },
-        empty: function(){
+    if(attic.supported()){
+        return attic;
+    }else{
+        console.log('localstorage not supported');
+        return false;
+    }
 
-        }
-
-    };
-    return attic;
 }());
